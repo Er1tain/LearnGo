98cec4f049d2419b4e547b9744ba85c7ccc4fe9b
Alexander Pridava <sanyapridava@mail.ru>
2024-04-03 13:58:39 +0300

    using buble sort

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"awesomeProject/myfuncs"
+	"awesomeProject/myfuncs/algs_sort"
+	"fmt"
+)
+
 type User struct {
 	UUID     int
 	surname  string
@@ -9,20 +15,60 @@ type User struct {
 }
 
 type Student struct {
-	student   User
-	code      string
-	course    int
-	group     int
-	faculties string
+	OutUniversityData User
+	code              string
+	course            int
+	group             int
+	faculties         string
 }
 
 type Teacher struct {
-	teacher      User
-	cafedra      string
-	faculties    string
-	science_rang string
+	OutUniversityData User
+	cafedra           string
+	faculties         string
+	science_rang      string
+}
+
+func f(value *int, x int) {
+	*value = x
+}
+
+func ArrayToString(arr []int) string {
+	res := ""
+	for _, arg := range arr {
+		res += string(arg) + " "
+	}
+
+	return res
+
 }
 
 func main() {
+	//x := 4
+	//value := &x
+
+	//fmt.Print(x)
+	//f(value, 12)
+	//fmt.Print(x)
+
+	//Изучим структуры данных
+
+	arr := []int{
+		1, 2, 3,
+	}
+
+	fmt.Println(arr)
+	fmt.Println(ArrayToString(arr))
+	myfuncs.Example()
+
+	//for _, arg := range arr {
+	//	fmt.Println(arg)
+	//}
+
+	//for i := 0; i < 10; i++ {
+	//	fmt.Println(i)
+	//}
+
+	algs_sort.Buble_sort([]float64{5, 1, -3, 3.2})
 
 }

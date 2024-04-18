8b8f9db605dc431a6a030b5f8ef4758d62868eb5
Alexander Pridava <sanyapridava@mail.ru>
2024-04-18 16:13:57 +0300

    Testing algs sort

--- main.go
+++ main.go
@@ -1,74 +1,13 @@
 package main
 
 import (
-	"awesomeProject/myfuncs"
 	"awesomeProject/myfuncs/algs_sort"
-	"fmt"
 )
 
-type User struct {
-	UUID     int
-	surname  string
-	name     string
-	patronym string
-	age      int
-}
-
-type Student struct {
-	OutUniversityData User
-	code              string
-	course            int
-	group             int
-	faculties         string
-}
-
-type Teacher struct {
-	OutUniversityData User
-	cafedra           string
-	faculties         string
-	science_rang      string
-}
-
-func f(value *int, x int) {
-	*value = x
-}
-
-func ArrayToString(arr []int) string {
-	res := ""
-	for _, arg := range arr {
-		res += string(arg) + " "
-	}
-
-	return res
-
-}
-
 func main() {
-	//x := 4
-	//value := &x
 
-	//fmt.Print(x)
-	//f(value, 12)
-	//fmt.Print(x)
+	arr := []float64{5, 1, -3, 3.2, 4.56, -12.34, 56, 12, -2.1}
 
-	//Изучим структуры данных
-
-	arr := []int{
-		1, 2, 3,
-	}
-
-	fmt.Println(arr)
-	fmt.Println(ArrayToString(arr))
-	myfuncs.Example()
-
-	//for _, arg := range arr {
-	//	fmt.Println(arg)
-	//}
-
-	//for i := 0; i < 10; i++ {
-	//	fmt.Println(i)
-	//}
-
-	algs_sort.Buble_sort([]float64{5, 1, -3, 3.2})
-
+	algs_sort.Buble_sort(arr)
+	//algs_sort.Include_sort(arr)
 }
